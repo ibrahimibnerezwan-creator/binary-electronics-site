099c438f1f958ff93d5a5b36354b978a99b4977e
Bipu <you@example.com>
2026-03-15 13:55:40 +0600

    Fix: JSX spread syntax, restore globals.css, add next-themes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -41,7 +41,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        ...props
+        {...props}
       />
     )
   }
